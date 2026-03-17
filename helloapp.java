46179dd766138dc657573ed781b891153a4789b1
aymaan3150 <am3150@srmist.edu.in>
2026-03-17 13:50:48 +0530

    Update helloapp.java

--- helloapp.java
+++ helloapp.java
@@ -1,5 +1,19 @@
 public class helloapp {
-    public static void main(String args[]){
-        System.out.println("Hello World");
+
+    public static void main(String[] args) {
+
+        String name;
+
+        // If no arguments, use default
+        if (args.length == 0) {
+            name = "World";
+        }
+
+        // If arguments exist, join all names
+        else {
+            name = String.join(", ", args);
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
